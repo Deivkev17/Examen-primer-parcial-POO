b35826c6ed66672162d7b68c0d1fc36e13cda8c5
user <user@user-PC>
2022-02-07 19:18:12 -0500

    Este es el proyecto presentado para la asignatura de POO

---
+++ src/ec/edu/intsuperior/modelo/Loggin.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author user
+ */
+public class Loggin {
+    private String user;
+    private String password;
+
+    public Loggin() {
+    }
+
+    public Loggin(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+    
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+    
+}
